e979e6b5578e3066610e3130625b852ee74dbcc8
Loy van Beek <loy.vanbeek@gmail.com>
2016-04-25 19:16:54 +0200

    find and replace function

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,2 +1,5 @@
-alias notebook="(pushd; cd ~/notebooks/ ; ipython notebook; popd)&"
+alias notebook="(pushd .; cd ~/notebooks/ ; ipython notebook; popd)&"
 alias resource="source ~/.bashrc"
+function find-replace {
+	ack-grep --print0 -irl '$1' | xargs -0 -L1 sed  -i 's/$1/$2/g'
+}
