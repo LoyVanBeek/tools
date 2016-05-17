1d990957f4ecf4c45e8f85515dd7be2ad3d9db1d
Loy <LoyVanBeek@users.noreply.github.com>
2016-05-17 21:20:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 sudo apt-get install vim terminator git  wget
 git clone https://github.com/LoyVanBeek/tools.git ~/tools`
 
-# echo 'source ~/tools/bash_aliases.sh' >> ~/.bashrc
+echo -e "\nsource ~/tools/bash_aliases.sh" >> ~/.bashrc
 
-# echo 'source ~/tools/git_terminal_setup.sh' >> ~/.bashrc
-
-# gnome-open "https://www.google.com/chrome"
+echo -e "\nsource ~/tools/git_terminal_setup.sh" >> ~/.bashrc
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+gnome-open "http://servicerobot.cstwiki.wtb.tue.nl/index.php?title=PC_Configuration#Ubuntu_14.04"
+
+git config --global credential.helper cache
+git config --global help.autocorrect 1
