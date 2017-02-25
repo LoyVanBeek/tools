a58d6e0275bf4b949b57cc161f6aa8c3f5725e86
Loy van Beek <loy.vanbeek@gmail.com>
2017-02-25 16:15:56 +0100

    Listing all the packages I want to install

--- packages.sh
+++ packages.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env sh
-sudo apt-get install ack-grep
+sudo apt-get install $(grep -vE "^\s*#" ~/tools/packages.list  | tr "\n" " ")
 
-sudo pip install pandas scipy
+sudo pip install -r requirements.txt
+
+# List all packages manually installed with 
+# $ comm -23 <(aptitude search '~i !~M' -F '%p' | sed "s/ *$//" | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) > tools/packages.txt
